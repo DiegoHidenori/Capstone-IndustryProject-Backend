0459bebab31762036cee5164defad30c9d74a2b2
Diego Tsukayama <diegohidenori.ca@hotmail.com>
2025-04-16 08:08:45 -0400

    Added SSL config for production DB in Sequelize CLI

--- src/config/config.js
+++ src/config/config.js
@@ -2,17 +2,23 @@ require("dotenv").config();
 // console.log("Loaded DB URL:", process.env.DATABASE_URL);
 
 module.exports = {
-    development: {
-        url: process.env.DATABASE_URL,
-        dialect: "postgres",
-        // schema: "public",
-    },
-    test: {
-        url: process.env.DATABASE_URL,
-        dialect: "postgres",
-    },
-    production: {
-        url: process.env.DATABASE_URL,
-        dialect: "postgres",
-    },
+	development: {
+		url: process.env.DATABASE_URL,
+		dialect: "postgres",
+		// schema: "public",
+	},
+	test: {
+		url: process.env.DATABASE_URL,
+		dialect: "postgres",
+	},
+	production: {
+		url: process.env.DATABASE_URL,
+		dialect: "postgres",
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false, // for Heroku self-signed certs
+			},
+		},
+	},
 };
